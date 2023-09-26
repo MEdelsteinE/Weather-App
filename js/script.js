29f65d79ce704b6aba28ba3beda27d3285cbe804
MedelsteinE <medelstein1123@gmail.com>
2023-09-26 18:07:47 -0400

    added logic to show todays weather and 5day forecast

--- js/script.js
+++ js/script.js
@@ -1,18 +1,46 @@
-var apiKey = "c40886ca6c36fc80578e039048174aeb"
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+$(function() {
+ $('#search-btn').on('click', function() {
+    var city = $('#city-input').val() 
+    todayApi(city);
+    //local storage then for loop to print them
+ })
+ https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+function todayApi (city) {
+  $.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=c40886ca6c36fc80578e039048174aeb&units=imperial`)
+  .then(function (data) {
+    
+    $("#city").text(data.name)
+    $("#temperature").text(data.main.temp)
+    $("#description").text(data.weather[0].description)
+    $("#humidity").text(data.main.humidity)
+    forecastApi(city)
+  });
+}
 
-$.ajax({
-    url: queryURL,
-    method: 'GET',
-    success: function(data) {
-        var weatherData = `
-      <p>Location: ${data.name}, ${data.sys.country}</p>
-      <p>Weather: ${data.weather[0].description}</p>
-      <p>Temperature: ${data.main.temp}°C</p>
-    `;
-    $('#weather-data').html(weatherData);
-  },
-  error: function(error) {
-    console.error('There was a problem with the API request:', error);
-  }
-});
+function forecastApi(city) {
+   $.get(`http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=c40886ca6c36fc80578e039048174aeb&units=imperial`)
+  .then(function (data) {
+    var weather = data.list
+    console.log(weather);
+
+    for (let i = 0; i < weather.length; i++) {
+      var time = weather[i].dt_txt.split(" ")[1]
+      if (time === "12:00:00") {
+        $("#forecast").append(
+          `
+          `
+        )
+      }
+    }
+
+
+  });
+}
+ 
+ 
+ 
+ 
+})
+
+
+
