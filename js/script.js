8d804ee96a5292e4a9d82cf39a99b678bb5b0f65
MedelsteinE <medelstein1123@gmail.com>
2023-09-27 00:09:02 -0400

    added local storage and display last city

--- js/script.js
+++ js/script.js
@@ -1,45 +1,53 @@
-$(function() {
- $('#search-btn').on('click', function() {
-    var city = $('#city-input').val() 
+$(function () {
+
+  $("#search-btn").on("click", function () {
+    var city = $("#city-input").val();
+    localStorage.setItem("previousCity", city);
     todayApi(city);
-    //local storage then for loop to print them
- })
- https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
-function todayApi (city) {
-  $.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=c40886ca6c36fc80578e039048174aeb&units=imperial`)
-  .then(function (data) {
-    
-    $("#city").text(data.name)
-    $("#temperature").text(data.main.temp)
-    $("#description").text(data.weather[0].description)
-    $("#humidity").text(data.main.humidity)
-    forecastApi(city)
   });
+
+
+
+var previousCity = localStorage.getItem("previousCity");
+if (previousCity) {
+  $("#previous-city").text(previousCity);
 }
 
-function forecastApi(city) {
-   $.get(`http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=c40886ca6c36fc80578e039048174aeb&units=imperial`)
-  .then(function (data) {
-    var weather = data.list
-    console.log(weather);
+  function todayApi(city) {
+    $.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=c40886ca6c36fc80578e039048174aeb&units=imperial`)
+      .then(function (data) {
 
-    for (let i = 0; i < weather.length; i++) {
-      var time = weather[i].dt_txt.split(" ")[1]
-      if (time === "12:00:00") {
-        $("#forecast").append(
+        $("#city").text(data.name)
+        $("#temperature").text(data.main.temp)
+        $("#description").text(data.weather[0].description)
+        $("#humidity").text(data.main.humidity)
+        forecastApi(city)
+      });
+  }
+
+  function forecastApi(city) {
+    $.get(`http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=c40886ca6c36fc80578e039048174aeb&units=imperial`)
+      .then(function (data) {
+        var weather = data.list
+        console.log(weather);
+
+        for (let i = 0; i < weather.length; i++) {
+          var time = weather[i].dt_txt.split(" ")[1]
+          if (time === "12:00:00") {
+            $("#forecast").append("<div>Append this div </div>"
+              `
           `
-          `
-        )
-      }
-    }
+            )
+          }
+        }
 
 
-  });
-}
- 
- 
- 
- 
+      });
+  }
+
+
+
+
 })
 
 
